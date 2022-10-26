8e843a5e6d529ca90e2b83d4dc2e7f67bd93f748
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2022-10-26 20:37:31 -0300

    feat: start using typescript on react storybook

--- packages/react/.storybook/main.js
+++ packages/react/.storybook/main.js
@@ -2,6 +2,15 @@ module.exports = {
   core: {
     builder: 'webpack5',
   },
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
+  },
   "stories": [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
