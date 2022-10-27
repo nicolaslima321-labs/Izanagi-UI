8a3a7063c56dc4cd9a8bb15dd780119bd35507d0
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2022-10-27 12:11:38 -0300

    release: @izanagi/core 0.0.0

--- packages/react/.storybook/main.js
+++ packages/react/.storybook/main.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   core: {
     builder: 'webpack5',
@@ -21,5 +23,33 @@ module.exports = {
     "@storybook/addon-interactions",
     '@storybook/preset-scss',
   ],
-  "framework": "@storybook/react"
+  "framework": "@storybook/react",
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.map((rule) => {
+      if (rule.oneOf) {
+        rule.oneOf = rule.oneOf.slice().map((subRule) => {
+          if (subRule.test instanceof RegExp && subRule.test.test('.scss')) {
+            return {
+              ...subRule,
+              use: [
+                ...subRule.use,
+                {
+                  loader: require.resolve('sass-resources-loader'),
+                  options: {
+                    resources: [
+                      path.resolve(__dirname, '../../core/src/styles/scss/globals.scss')
+                    ]
+                  }
+                }
+              ],
+            }
+          }
+          return subRule;
+        });
+      }
+      return rule;
+    });
+
+    return config;
+  },
 }
